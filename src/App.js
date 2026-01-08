19ddf58b787a62fbf9c4ee97657397df985316eb
bitturohit <rohit97750@gmail.com>
2026-01-08 17:01:33 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import Button from "./Button";
+
+function App() {
+   return (
+      <div>
+         <div>
+            <Button primary success rounded outline>
+               Click me!!
+            </Button>
+         </div>
+         <div>
+            <Button>Buy Now!</Button>
+         </div>
+         <div>
+            <Button>See Deal!</Button>
+         </div>
+         <div>
+            <Button>Hide Ads!</Button>
+         </div>
+         <div>
+            <Button>Something!</Button>
+         </div>
+      </div>
+   );
+}
+
+export default App;
