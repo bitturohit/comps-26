ddd1d3ab3166dc6232586ad96c629fa425e1e9b1
bitturohit <rohit97750@gmail.com>
2026-03-11 15:56:44 +0530

    modal close

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,5 +1,18 @@
-function Modal() {
-   return <div>modal!</div>;
+import { createPortal } from "react-dom";
+
+function Modal({ onClose }) {
+   return createPortal(
+      <div>
+         <div
+            className="absolute inset-0 bg-gray-300 opacity-80"
+            onClick={onClose}
+         ></div>
+         <div className="absolute inset-x-40 inset-y-60 p-10 bg-white">
+            I am a Modal!!
+         </div>
+      </div>,
+      document.querySelector(".modal-container"),
+   );
 }
 
 export default Modal;
--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -1,9 +1,21 @@
+import { useState } from "react";
+
 import Modal from "../components/Modal";
+import Button from "../components/Button";
 
 function ModalPage() {
+   const [showModal, setShowModal] = useState(false);
+
+   const handleClick = () => {
+      setShowModal(!showModal);
+   };
+
    return (
       <div>
-         <Modal />
+         <Button primary onClick={handleClick}>
+            Open Modal
+         </Button>
+         {showModal && <Modal onClose={handleClick} />}
       </div>
    );
 }
