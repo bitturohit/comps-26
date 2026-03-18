179108f8dcd38198e085ddce789a4915cafb338a
bitturohit <rohit97750@gmail.com>
2026-03-18 19:03:17 +0530

    fixed cell values

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,22 +1,22 @@
-function Table({ data }) {
+function Table({ data, config }) {
+   const renderedHeaders = config.map((column) => {
+      return <th key={column.label}>{column.label}</th>;
+   });
+
    const renderedRows = data.map((fruit) => {
       return (
-         <tr key={fruit.name}>
-            <td>{fruit.name}</td>
-            <td>{fruit.color}</td>
-            <td>{fruit.score}</td>
+         <tr key={fruit.name} className="border-b border-gray-200">
+            <td className="p-3">{config[0].render(fruit)}</td>
+            <td className="p-3">{config[1].render(fruit)}</td>
+            <td className="p-3">{config[2].render(fruit)}</td>
          </tr>
       );
    });
 
    return (
-      <table>
+      <table className="table-auto border-spacing-2">
          <thead>
-            <tr>
-               <th>Fruit</th>
-               <th>Color</th>
-               <th>Score</th>
-            </tr>
+            <tr className="border-b-2 border-gray-300">{renderedHeaders}</tr>
          </thead>
          <tbody>{renderedRows}</tbody>
       </table>
--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -4,12 +4,19 @@ function TablePage() {
    const data = [
       { name: "Orange", color: "bg-orange-500", score: 5 },
       { name: "Apple", color: "bg-red-500", score: 3 },
-      { name: "Banana", color: "bg-yellow-500", score: 1 },
-      { name: "Lime", color: "bg-green-500", score: 4 },
+      { name: "Banana", color: "bg-yellow-300", score: 1 },
+      { name: "Lime", color: "bg-green-200", score: 4 },
    ];
+
+   const config = [
+      { label: "Name of Fruit", render: (fruit) => fruit.name },
+      { label: "Color", render: (fruit) => fruit.color },
+      { label: "Score", render: (fruit) => fruit.score },
+   ];
+
    return (
       <div>
-         <Table data={data} />
+         <Table data={data} config={config} />
       </div>
    );
 }
