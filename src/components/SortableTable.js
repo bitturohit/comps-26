dec5d500d5050744aa6d8c80be6344bb86f85c14
bitturohit <rohit97750@gmail.com>
2026-04-01 19:41:46 +0530

    sorting complete

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,7 +1,98 @@
+import { useState } from "react";
+import { GoArrowSmallDown, GoArrowSmallUp } from "react-icons/go";
+
 import Table from "./Table";
 
 function Sortabletable(props) {
-   return <Table {...props} />;
+   const [sortOrder, setSortOrder] = useState(null);
+   const [sortBy, setSortBy] = useState(null);
+
+   const { config, data } = props;
+
+   const handleClick = (label) => {
+      if (sortBy && label !== sortBy) {
+         setSortOrder("asc");
+         setSortBy(label);
+         return;
+      }
+
+      if (sortOrder === null) {
+         setSortOrder("asc");
+         setSortBy(label);
+      } else if (sortOrder === "asc") {
+         setSortOrder("desc");
+         setSortBy(label);
+      } else if (sortOrder === "desc") {
+         setSortOrder(null);
+         setSortBy(null);
+      }
+   };
+
+   const updatedConfig = config.map((column) => {
+      if (!column.sortValue) {
+         return column;
+      }
+
+      return {
+         ...column,
+         header: () => (
+            <th
+               className="cursor-pointer hover:bg-gray-100"
+               key={column.label}
+               onClick={() => handleClick(column.label)}
+            >
+               <div className="flex items-center">
+                  {getIcons(column.label, sortBy, sortOrder)}
+                  {column.label}
+               </div>
+            </th>
+         ),
+      };
+   });
+
+   let sortedData = data;
+   if (sortOrder && sortBy) {
+      const { sortValue } = config.find((column) => column.label === sortBy);
+      const reverseOrder = sortOrder === "asc" ? 1 : -1;
+
+      sortedData = [...data].sort((a, b) => {
+         const valueA = sortValue(a);
+         const valueB = sortValue(b);
+
+         if (typeof valueA === "string") {
+            return valueA.localeCompare(valueB) * reverseOrder;
+         } else {
+            return (valueA - valueB) * reverseOrder;
+         }
+      });
+   }
+
+   return <Table {...props} data={sortedData} config={updatedConfig} />;
+}
+
+function getIcons(label, sortBy, sortOrder) {
+   if (label !== sortBy) {
+      return (
+         <div>
+            <GoArrowSmallUp />
+            <GoArrowSmallDown />
+         </div>
+      );
+   }
+
+   if (sortOrder === "asc") {
+      return (
+         <div>
+            <GoArrowSmallUp />
+         </div>
+      );
+   } else if (sortOrder === "desc") {
+      return (
+         <div>
+            <GoArrowSmallDown />
+         </div>
+      );
+   }
 }
 
 export default Sortabletable;
--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -9,7 +9,11 @@ function TablePage() {
    ];
 
    const config = [
-      { label: "Name", render: (fruit) => fruit.name },
+      {
+         label: "Name",
+         render: (fruit) => fruit.name,
+         sortValue: (fruit) => fruit.name,
+      },
       {
          label: "Color",
          render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`} />,
@@ -17,7 +21,8 @@ function TablePage() {
       {
          label: "Score",
          render: (fruit) => fruit.score,
-         header: () => <th className="bg-red-300 px-2">Score</th>,
+         sortValue: (fruit) => fruit.score,
+         // header: () => <th className="bg-red-300 px-2">Score</th>,
       },
       { label: "Score Squared", render: (fruit) => fruit.score ** 2 },
    ];
